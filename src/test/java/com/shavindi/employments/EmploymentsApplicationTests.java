962037d2b39ef9626fe99371ed5e1ac969627a4c
ShavindiSanthupamaLiyanage <shavis1113@gmail.com>
2024-10-29 05:26:58 +0530

    first commit

---
+++ src/test/java/com/shavindi/employments/EmploymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shavindi.employments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmploymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
